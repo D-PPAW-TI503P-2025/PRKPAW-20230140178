f8da22f37d05ef16bfc6bbcf6a51ad0c3b3e857e
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-11 17:37:02 +0700

    first commit

---
+++ 20230140178-server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
+// Route utama
+app.get('/', (req, res) => {
+  res.send('Home Page for API');
+});
+
+// Import router dari folder routes
+const bookRoutes = require('./routes/books');
+app.use('/api/books', bookRoutes);
+
+// Middleware 404 (Not Found)
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
---
+++ Tugas-2/README.md
@@ -0,0 +1,19 @@
+# CRUD Aplikasi
+
+### 1. Create (Tambah Data)
+Menampilkan form untuk menambahkan data baru ke dalam sistem.
+![Create](ssan/Create.png)
+
+### 2. Read (Lihat Data)
+Menampilkan daftar data yang sudah tersimpan di dalam database.
+![Lihat](ssan/Lihat.png)
+
+### 3. Update (Ubah Data)
+Memungkinkan pengguna mengubah data yang sudah ada.
+![Update](ssan/update.png)
+
+### 4. Delete (Hapus Data)
+Fitur untuk menghapus data yang tidak dibutuhkan lagi.
+![Hapus](ssan/Hapus.png)
+
+
